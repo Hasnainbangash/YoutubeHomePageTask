3068c653c8ddb4c8831af5aa740c446093059966
Bangash38 <bangashmhasnain38@gmail.com>
2025-02-15 19:32:50 +0500

    Initial Commit

---
+++ YoutubeHomePageTask/YoutubeHomePageTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeHomePageTaskApp.swift
+//  YoutubeHomePageTask
+//
+//  Created by Muhammad Hasnain Bangash on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeHomePageTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YoutubeHomePageTaskTests/YoutubeHomePageTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeHomePageTaskTests.swift
+//  YoutubeHomePageTaskTests
+//
+//  Created by Muhammad Hasnain Bangash on 15/02/2025.
+//
+
+import Testing
+@testable import YoutubeHomePageTask
+
+struct YoutubeHomePageTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
